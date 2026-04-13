af1116815fa97417ceb300d0853839e6bb966e66
Андрей <ruller.17.23@gmail.com>
2026-04-13 17:09:59 +0300

    Fix conflicts playwrite

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  // Теперь Allure подключается как среда выполнения, а не репортер
   testEnvironment: "allure-jest/node",
   testEnvironmentOptions: {
     resultsDir: "allure-results"
-  }
+  },
+  // Строго говорим Jest: ищи тесты ТОЛЬКО в папке tests/unit и только с окончанием .test.js
+  testMatch: [
+    "**/tests/unit/**/*.test.js"
+  ]
 };
